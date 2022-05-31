d3f0107a31c5548ce58761b6d532de4dfaeb200e
RanaMuhammadIrshad <rmirshad@web.de>
2022-05-31 11:51:48 +0200

    Task are completed related to functions in Udemy course

--- solution.js
+++ solution.js
@@ -1,3 +1,4 @@
+'use strict';
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -32,6 +33,48 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  answers: new Array(4).fill(0),
+  // get answer
+  registerNewAnswer: function () {
+    let myArr = [];
+    const answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.join('\n')}\n(write option number)`
+      )
+    );
+    console.log(answer);
+    //register answer
+
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
 ///////////////////////////////////////
 // Coding Challenge #2
 
@@ -44,3 +87,12 @@ And now explain to YOURSELF (or someone around you) WHY this worked! Take all th
 
 GOOD LUCK 😀
 */
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
